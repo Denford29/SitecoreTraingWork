0a3a457b5c4fcc3379bd5908d55dda909eb81785
Denford29 <dennydante@gmail.com>
2017-08-03 17:02:59 +0930

    Initial build of SATC Framework

---
+++ events.tac.local/Controllers/PageHeaderController.cs
@@ -0,0 +1,41 @@
+﻿using events.tac.local.Models;
+using SATC.SC.Framework.SitecoreHelpers;
+using Sitecore.Data;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class PageHeaderController : Controller
+    {
+        /// <summary>
+        /// Get the default menu items and other settings
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //get the master database to use to get items from
+            var database = Sitecore.Configuration.Factory.GetDatabase("master");
+
+            //create a new model
+            var model = new PageHeaderModel();
+
+            // get the default home page to use to get the menu items
+            var homePage = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+
+            // get the homepage id set in the config
+            var homePageId = StandardHelper.GetItemIDFromConfig("HomePageID");
+            if(homePageId != ID.Null)
+            {
+                var databaseHomePage = database.GetItem(homePageId);
+                if(databaseHomePage != null)
+                {
+                    homePage = databaseHomePage;
+                }
+            }
+
+            //
+
+            return View();
+        }
+    }
+}
---
+++ events.tac.local/Models/PageHeaderModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Models
+{
+    public class PageHeaderModel
+    {
+        /// <summary>
+        /// get or set a list of child navigation items
+        /// </summary>
+        public IList<MenuItemModel> MenuItems
+        {
+            get;
+            set;
+        }
+        = new List<MenuItemModel>();
+
+        /// <summary>
+        /// get or set the logo image html string
+        /// </summary>
+        public HtmlString LogoImage
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// get or set the list of languages available
+        /// </summary>
+        public IList<SelectListItem> Languages
+        {
+            get;
+            set;
+        }
+    }
+}
